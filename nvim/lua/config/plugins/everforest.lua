82953291a97c3b8040015db743f75a971895a793
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-02-06 14:59:33 +0100

    refactor: changed nvim configuration from single-file to multi-file

---
+++ nvim/lua/config/plugins/everforest.lua
@@ -0,0 +1,11 @@
+return {
+	"neanias/everforest-nvim",
+	version = false,
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("everforest").setup({
+			disable_italic_comments = true,
+		})
+	end,
+}

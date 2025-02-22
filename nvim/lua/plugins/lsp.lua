4c25d634bb2133d548e972a695ce76e8d08e6c29
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-02-22 12:21:44 +0100

    feature(nvim): added lua files lsp and mason

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,10 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      emmet_language_server = {
+        filetypes = { "html", "css", "sass", "scss" },
+      },
+    },
+  },
+}
---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,21 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "emmet-language-server",
+      "eslint-lsp",
+      "json-lsp",
+      "lua-language-server",
+      "markdown-toc",
+      "markdownlint-cli2",
+      "marksman",
+      "prettier",
+      "shfmt",
+      "stylua",
+      "tailwindcss-language-server",
+      "taplo",
+      "vtsls",
+      "yaml-language-server",
+    },
+  },
+}

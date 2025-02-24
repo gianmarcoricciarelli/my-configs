0e1b03240cbe42977d68af260d1452862aca412b
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-02-24 11:01:01 +0100

    feature(nvim): added keymappings for moving a line up or down

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-S-Down>", "<cmd>m+1<CR>")
+vim.keymap.set("n", "<C-S-Up>", "<cmd>m-2<CR>")
--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,14 +1,14 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    picker = {
-      hidden = true,
-      ignored = true,
-      sources = {
-        files = {
-          hidden = true,
+    "folke/snacks.nvim",
+    opts = {
+        picker = {
+            hidden = true,
+            ignored = true,
+            sources = {
+                files = {
+                    hidden = true,
+                },
+            },
         },
-      },
     },
-  },
 }

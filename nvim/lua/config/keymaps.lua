471445e5d0ed79070c3332add61f25558b45f561
Gianmarco Ricciarelli <gianmarcoricciarelli@gmail.com>
2025-03-14 15:26:25 +0100

    fix(nvim): Fixed key-mappings
    Fix key-mappings for moving block of code up and down both in normal
    mode and in visual mode.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,7 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-S-Down>", "<cmd>m+1<CR>")
-vim.keymap.set("v", "<C-S-Down>", "<cmd>m+1<CR>")
-vim.keymap.set("n", "<C-S-Up>", "<cmd>m-2<CR>")
-vim.keymap.set("v", "<C-S-Up>", "<cmd>m-2<CR>")
+-- Upper comment
+-- More upper comment
+-- Move selected lines down
+vim.keymap.set("v", "<C-S-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+-- Move selected lines up
+vim.keymap.set("v", "<C-S-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+-- Move single lines in normal mode
+vim.keymap.set("n", "<C-S-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+-- A comment
+-- Another comment
